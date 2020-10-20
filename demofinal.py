87533c6f994ced90b2d53c91f7c9436dc0185914
Ilmari T <iltalo@live.com>
2020-10-20 21:24:33 +0300

    cleaned data

--- demofinal.py
+++ demofinal.py
@@ -17,7 +17,6 @@ from ast import literal_eval
 app = Flask(__name__)
 
 clusters = pd.read_csv("huutoClusters.csv", converters = {"cTitle": literal_eval, "links": literal_eval})
-clusters = clusters.drop(index = 4)
 
 ###Search algo
 
